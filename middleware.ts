77a9da9f46609f2ae097072495f328ad3b23be27
minhhieu298 <hieuti2908@gmail.com>
2025-12-03 23:26:04 +0700

    middle wware

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,21 @@ import { NextResponse } from "next/server";
 
 export function middleware() {
   const response = NextResponse.next();
-  if (process.env.NODE_ENV === "production") {
-    response.headers.append(
-      "Set-Cookie",
-      `aspfpt_sessiontoken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; `
-    );
-    response.cookies.set({
-      name: "aspfpt_sessiontoken",
-      expires: new Date(0),
-      value: "",
-      path: "/",
-    });
+  // if (process.env.NODE_ENV === "production") {
+  //   response.headers.append(
+  //     "Set-Cookie",
+  //     `aspfpt_sessiontoken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; `
+  //   );
+  //   response.cookies.set({
+  //     name: "aspfpt_sessiontoken",
+  //     expires: new Date(0),
+  //     value: "",
+  //     path: "/",
+  //   });
 
-    // Delete a cookie
-    // response.cookies.delete("aspfpt_sessiontoken");
-  }
+  //   // Delete a cookie
+  //   // response.cookies.delete("aspfpt_sessiontoken");
+  // }
 
   return response;
 }
